226a890d8240a8bb7955ce9205d497ada0d40361
keithkattt <k3ith4rwil@gmail.com>
2025-01-08 21:13:02 +0800

    Added cake and js for cake

---
+++ myscript.js
@@ -0,0 +1,7 @@
+function changeImageAndText() {
+    // Change the image
+    document.getElementById('cakeImage').src = 'img/birthdayCakeBlowed.png';
+    
+    // Change the text
+    document.getElementById('imageText').innerText = 'Happy Birthday!';
+  }
